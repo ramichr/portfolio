7edca95c23f09b309cdbe9cd8118405525838fdd
Rami Cheikh Rouhou <ramicheikhrouhou670@gmail.com>
2022-02-26 14:39:27 +0100

    managing Nav states

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,25 +3,63 @@ import { AiOutlineContacts, AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
 import { FaGraduationCap } from 'react-icons/fa'
 import { BiBook } from 'react-icons/bi'
 import { BsPalette } from 'react-icons/bs'
+import { useState } from 'react'
 export default function Nav() {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href='#' className='active'>
+      <a
+        href='#'
+        onClick={() => {
+          setActiveNav('#')
+        }}
+        className={activeNav === '#' ? 'active' : ''}
+      >
         <AiOutlineHome />
       </a>
-      <a href='#about'>
+      <a
+        href='#about'
+        onClick={() => {
+          setActiveNav('#about')
+        }}
+        className={activeNav === '#about' ? 'active' : ''}
+      >
         <AiOutlineUser />
       </a>
-      <a href='#education'>
+      <a
+        href='#education'
+        onClick={() => {
+          setActiveNav('#education')
+        }}
+        className={activeNav === '#education' ? 'active' : ''}
+      >
         <FaGraduationCap />
       </a>
-      <a href='#skills'>
+      <a
+        href='#skills'
+        onClick={() => {
+          setActiveNav('#skills')
+        }}
+        className={activeNav === '#skills' ? 'active' : ''}
+      >
         <BiBook />
       </a>
-      <a href='#portfolio'>
+      <a
+        href='#portfolio'
+        onClick={() => {
+          setActiveNav('#portfolio')
+        }}
+        className={activeNav === '#portfolio' ? 'active' : ''}
+      >
         <BsPalette />
       </a>
-      <a href='#contact'>
+      <a
+        href='#contact'
+        onClick={() => {
+          setActiveNav('#contact')
+        }}
+        className={activeNav === '#contact' ? 'active' : ''}
+      >
         <AiOutlineContacts />
       </a>
     </nav>
